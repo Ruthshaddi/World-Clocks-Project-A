3d9aadbd24ea8c44934f39ddc6dc935f6f34ad3e
Ruthshaddi <odusanyaruth26@gmail.com>
2024-12-21 19:13:53 +0100

    Added current location to the project

--- index.js
+++ index.js
@@ -34,10 +34,15 @@ if (polandElement) {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+
+
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML += `
+  citiesElement.innerHTML = `
   <div class="city">
     <div>
       <h2>${cityName}</h2>
